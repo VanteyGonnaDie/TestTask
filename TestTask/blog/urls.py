ac3e2c268afad6914dfc65a08c0a377f163e77e9
VanteyGonnaDie <bahya.d97@gmail.com>
2020-12-23 15:54:13 +0300

    Добавление/удаление постов, авторизация

--- TestTask/blog/urls.py
+++ TestTask/blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
  
-from .views import BlogDetailView, BlogListView
+from .views import BlogDeleteView, BlogDetailView, BlogListView, BlogCreateView
  
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
-    path('blog/<int:pk>/', BlogDetailView.as_view(), name='blog_detail')
+    path('blog/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
+    path('blog/new', BlogCreateView.as_view(), name='blog_new' ),
+    path('blog/<int:pk>/delete', BlogDeleteView.as_view(), name='blog_delete')
 ]
--- TestTask/blog/views.py
+++ TestTask/blog/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Blog,BlogAuthor
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, DeleteView
+from django.urls import reverse_lazy
 
  
  
@@ -12,3 +12,13 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Blog
     template_name = 'blog_detail.html'
+
+class BlogCreateView(CreateView):
+    model = Blog
+    template_name = 'blog_new.html'
+    fields = ['title','author','blog_text','blog_date']
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    template_name = 'blog_delete.html'
+    success_url = reverse_lazy('home')
