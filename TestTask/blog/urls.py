14ad622f0442a28ca491a7731ec10b9c7845f9d2
VanteyGonnaDie <bahya.d97@gmail.com>
2020-12-23 17:27:49 +0300

    Список всех авторов/посты одного пользователя

--- TestTask/blog/urls.py
+++ TestTask/blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
  
-from .views import BlogDeleteView, BlogDetailView, BlogListView, BlogCreateView
+from .views import BlogDeleteView, BlogDetailView, BlogListView, BlogCreateView, BloggersList, BlogsByAuthor
  
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
     path('blog/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
     path('blog/new', BlogCreateView.as_view(), name='blog_new' ),
-    path('blog/<int:pk>/delete', BlogDeleteView.as_view(), name='blog_delete')
+    path('blog/<int:pk>/delete', BlogDeleteView.as_view(), name='blog_delete'),
+    path('bloggers/', BloggersList.as_view(), name='bloggers'),
+    path('bloggers/<int:pk>/', BlogsByAuthor.as_view(), name='blogs_by_author')
 ]
--- TestTask/blog/views.py
+++ TestTask/blog/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from .models import Blog,BlogAuthor
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, DeleteView
@@ -22,3 +23,15 @@ class BlogDeleteView(DeleteView):
     model = Blog
     template_name = 'blog_delete.html'
     success_url = reverse_lazy('home')
+
+class BloggersList(ListView):
+    model = BlogAuthor
+    template_name = 'bloggers.html'
+
+class BlogsByAuthor(ListView):
+    model = Blog
+    template_name = 'blogs_by_author.html'
+    def get_queryset(self):
+        id =self.kwargs['pk']
+        target = get_object_or_404(BlogAuthor, pk=id)
+        return Blog.objects.filter(author=target)
