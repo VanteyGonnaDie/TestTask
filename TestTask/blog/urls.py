45695baf0aefb55f7655bf023866c218dfea86fe
VanteyGonnaDie <bahya.d97@gmail.com>
2020-12-23 15:06:13 +0300

    Исправления, минимальный html/css, view функции

--- TestTask/blog/urls.py
+++ TestTask/blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
  
-from .views import BlogListView
+from .views import BlogDetailView, BlogListView
  
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
+    path('blog/<int:pk>/', BlogDetailView.as_view(), name='blog_detail')
 ]
--- TestTask/blog/views.py
+++ TestTask/blog/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Blog,BlogAuthor
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
  
  
 class BlogListView(ListView):
     model = Blog
     template_name = 'home.html'
+
+class BlogDetailView(DetailView):
+    model = Blog
+    template_name = 'blog_detail.html'
